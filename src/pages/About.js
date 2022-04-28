108b7b963c2ff61dd3983a6d23d5befe9c5752b5
Taylor Griffin <tmgriffin73@gmail.com>
2022-04-28 14:23:33 -0400

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <section className="section about-section">
+        <h1 className="section-title">about us</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+          possimus corrupti perferendis aspernatur nobis fugiat quisquam dicta
+          incidunt deleniti asperiores. Dignissimos, ea quibusdam possimus
+          optio, eveniet illum dicta dolorum ratione excepturi ullam quam
+          perspiciatis velit similique. Voluptatibus recusandae incidunt atque!
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default About;
